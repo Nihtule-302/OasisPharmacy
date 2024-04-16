b9de6860f602992a0b1fbda3819251dd8a04b160
Ali wafa <30670268+Nihtule-302@users.noreply.github.com>
2024-04-16 21:10:28 +0200

    .

--- app/Models/OrderdItem.php
+++ app/Models/OrderdItem.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OrderdItem extends Model
 {
@@ -13,4 +14,14 @@ class OrderdItem extends Model
         'order_id',
     ];
     use HasFactory;
+
+    public function pharmaceuticalProduct(): BelongsTo
+    {
+        return $this->belongsTo(PharmaceuticalProduct::class);
+    }
+    
+    /*public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }*/
 }
--- app/Models/PharmaceuticalProduct.php
+++ app/Models/PharmaceuticalProduct.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class PharmaceuticalProduct extends Model
 {
@@ -15,4 +16,10 @@ class PharmaceuticalProduct extends Model
         'description',
     ];
     use HasFactory;
+
+    public function orderdItem(): HasMany
+    {
+        return $this->hasMany(OrderdItem::class);
+    }
+
 }
