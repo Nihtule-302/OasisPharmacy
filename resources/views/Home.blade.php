07b809925fb992e59231c0cf3bc2e886cba33283
SaifMostafa0 <saifallah3@gmail.com>
2024-04-21 19:10:53 +0200

    Update Home.blade.php

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -16,8 +16,8 @@
                             <p class="card-text">Oasis Pharmacy was founded with a vision to be an oasis of health and wellness in our community. The name "Oasis" reflects our commitment to providing a refreshing and rejuvenating experience for our customers.</p>
                             <p class="card-text">At Oasis Pharmacy, we believe that health is a journey, and we are here to support you every step of the way. Whether it's filling your prescriptions, offering expert advice, or providing essential healthcare products, we strive to be your trusted partner in health.</p>
                             <p class="card-text">Our name embodies our mission to be a sanctuary of care and comfort, where you can find relief and replenishment for your health needs. We invite you to experience the difference at Oasis Pharmacy.</p>
-                        </div>
-                        <div class="card-body bg-white-opacity">
+                        
+                        
                             <h3 class="card-title">Contact Us</h3>
                             <p class="card-text">If you have any questions or would like to learn more about our services, feel free to reach out to us:</p>
                             <ul class="list-unstyled">
