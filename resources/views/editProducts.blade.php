9a77eafe3f0c3d58c9409a66d1a8307662f5e3ca
peterelia22 <peterelia914@gmail.com>
2024-05-08 20:17:47 +0300

    edit product button

--- resources/views/editProducts.blade.php
+++ resources/views/editProducts.blade.php
@@ -108,7 +108,7 @@
                             <div class="row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('Add Product') }}
+                                        {{ __('Edit Product') }}
                                     </button>
                                 </div>
                             </div>
